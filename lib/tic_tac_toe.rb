e7b34d22bfed2a3a255dd6992a559c84ca1d5816
Travis Clarkson <tk1667@gmail.com>
2018-02-12 22:14:54 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,93 @@
+class TicTacToe
+  def initialize
+    @board = Array.new(9, " ")
+    puts "Welcome to Tic Tac Toe!"
+    display_board
+  end
+
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    @index = input.to_i - 1
+  end
+
+  def move(index, token = "X")
+    @board[index] = token
+  end
+
+  def position_taken?(index)
+    @board[index] == "X" || @board[index] == "O"
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+
+  def turn_count
+    @board.count{|i| i == "X" || i == "O"}
+  end
+
+  def current_player
+    turn_count.even? ? "X" : "O"
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    valid_move?(index) ? move(index, current_player) : turn
+    display_board
+  end
+
+  def won?
+    WIN_COMBINATIONS.find do |combo|
+      combo.all? {|i| @board[i] == "X"} ||
+      combo.all? {|i| @board[i] == "O"}
+    end
+  end
+
+  def full?
+    @board.all?{|i| i == "X" || i == "O"}
+  end
+
+  def draw?
+    full? && !won?
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def winner
+    won? ? @board[won?.first] : nil
+  end
+
+  def play
+    until over?
+      turn
+    end
+    if draw?
+      puts "Cat's Game!"
+    else
+      puts "Congratulations #{winner}!"
+    end
+  end
+
+end
